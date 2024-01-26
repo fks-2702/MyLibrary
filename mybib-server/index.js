4d15886bb865b7dba7f262c85c7b970f7ef37271
fks <f4ferris@gmail.com>
2024-01-25 19:38:48 -0500

    test

--- mybib-server/index.js
+++ mybib-server/index.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const axios = require("axios");
 const sqlite3 = require('sqlite3').verbose();
+
+
 const db = new sqlite3.Database('./db/books.db');
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const app = express();
 app.use(express.json());
+
 app.use(cors());
 
 
@@ -24,7 +27,51 @@ app.get("/api", (req, res) => {
     res.json({ message: "I'm aliveeee" });
 });
 
-db.run(`CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, isbn TEXT, cover BLOB)`);
+db.run(`CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, isbn TEXT, cover BLOB, pages INTEGER, genre TEXT)`);
+
+
+app.get("/api/stats", (req, res) => {
+    
+    db.all('SELECT genre, COUNT(*) AS count FROM books GROUP BY genre ORDER BY count DESC', (err, genreRows) => {
+        if (err) {
+            throw err;
+        }
+    
+        console.log("Genre Rows:", genreRows);
+    
+        const genresArray = genreRows.map(row => ({ genre: row.genre, count: row.count }));
+        
+        console.log("Genres Array:", genresArray);
+        
+        console.log("Genre Rows:", genreRows[0]);
+
+        db.all('SELECT pages, COUNT(*) AS count FROM books GROUP BY pages', (err, pagesRows) => {
+            if (err) {
+                throw err;
+            }
+
+            let sum = 0;
+            let totalCount = 0;
+
+            for (let i = 0; i < pagesRows.length; i++) {
+                sum += pagesRows[i].pages * pagesRows[i].count;
+                totalCount += pagesRows[i].count;
+            }
+
+            const averagePages = sum / totalCount;
+            console.log(averagePages)
+
+            const result = {
+                genres: genreRows,
+                avg: averagePages
+            };
+
+            res.json(result);
+        });
+    });
+});
+
+
 
 app.post("/api/addbook", async (req, res) => {
     console.log(req.body);
@@ -34,25 +81,29 @@ app.post("/api/addbook", async (req, res) => {
         }
 
         // Fetch book data from Open Library API
-        const response = await axios.get(`https://openlibrary.org/isbn/${req.body.isbn}.json`);
-        const coverrequest = await axios.get(`https://covers.openlibrary.org/b/isbn/${req.body.isbn}-M.jpg`);
-        var bookData = response.data;
-        const author = await axios.get(`https://openlibrary.org/${bookData.authors[0].key}.json`);
-        bookData.author = author.data.name;
-        const cover = coverrequest.data;
-        console.log(bookData);
-        db.run(`INSERT INTO books (title, author, isbn, cover) VALUES (?, ?, ?, ?)`, [bookData.title, bookData.author, req.body.isbn,cover], function(err) {
-            if (err) {
-                console.log(err.message);
-                return res.status(500).json({ message: "Error adding book" });
-            }
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${req.body.isbn}`);
+        const coverRequest = await axios.get(`https://covers.openlibrary.org/b/isbn/${req.body.isbn}-M.jpg`);
+        const bookData = response.data.items[0].volumeInfo;
+        const cover = coverRequest.data;
 
-            console.log(`${bookData.title} added`);
-            res.json({ message: `${bookData.title} added`});
-        });
+        console.log(bookData);
+
+        db.run(
+            `INSERT INTO books (title, author, isbn, cover, pages, genre) VALUES (?, ?, ?, ?, ?, ?)`,
+            [bookData.title, bookData.authors[0], req.body.isbn, cover, bookData.pageCount, bookData.categories ? bookData.categories[0] : null],
+            function (err) {
+                if (err) {
+                    console.log(err.message);
+                    return res.status(500).json({ message: "Error adding book" });
+                }
+
+                console.log(`${bookData.title} added`);
+                res.json({ message: `${bookData.title} added` });
+            }
+        );
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "Error fetching book data" });
+        res.status(500).json({ message: "Error adding or fetching book data" });
     }
 });
 
